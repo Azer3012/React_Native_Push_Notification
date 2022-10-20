383ee29524bfaa55666a65277f874de452eff2de
azer <azer@azers-MacBook-Pro.local>
2022-10-20 21:49:29 +0400

    android

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { notificationListener, requestUserPermission } from './src/utils/pushnotification_helper'
 
 const App = () => {
+
+  useEffect(()=>{
+    requestUserPermission()
+    notificationListener()
+  },[])
   return (
     <View>
       <Text>App</Text>
--- src/utils/pushnotification_helper.js
+++ src/utils/pushnotification_helper.js
@@ -1,6 +1,7 @@
 import messaging from '@react-native-firebase/messaging';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-async function requestUserPermission() {
+
+export  const  requestUserPermission= async()=> {
   const authStatus = await messaging().requestPermission();
   const enabled =
     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
@@ -8,5 +9,51 @@ async function requestUserPermission() {
 
   if (enabled) {
     console.log('Authorization status:', authStatus);
+    getFCMToken()
   }
+}
+
+const getFCMToken=async ()=>{
+    let fcmToken=await AsyncStorage.getItem("fcmToken")
+
+    console.log({oldToken:fcmToken});
+    if(!fcmToken){
+        try {
+            let fcmToken=await messaging().getToken()
+            console.log({newToken:fcmToken});
+            if(fcmToken){
+                AsyncStorage.setItem("fcmToken",fcmToken)
+            }
+        } catch (error) {
+            
+        }
+    }
+}
+
+
+export const notificationListener=()=>{
+    messaging().onNotificationOpenedApp(remoteMessage => {
+        console.log(
+          'Notification caused app to open from background state:',
+          remoteMessage.notification,
+        );
+        
+      });
+
+      messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+          
+        }
+       
+      });
+
+      messaging().onMessage(async (remoteMessage)=>{
+        console.log("notification on froground state....",remoteMessage);
+      })
 }
